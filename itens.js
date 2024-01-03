fb3be354bfa5a95b67bb012bfce56a1802cd0742
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-01-03 10:55:52 -0300

    att

---
+++ itens.js
@@ -0,0 +1,16 @@
+
+const ITENS = {
+
+    'burger':{
+        name:"Burger",
+        picture:'images/burguer.jpg'
+    },
+    'feijoada':{
+        name:"Feijoada",
+        picture:'images/feijoada.jpg'
+    },
+    'pizza':{
+        name:"Pizza",
+        picture:'images/pizza.jpg'
+    }
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+/**@type {Element404}*/
+let main_interface;
+
+function start(){
+
+
+}
+function main(){
+    main_interface = createElement404(start,document.body);
+    main_interface.render();
+
+}
+window.addEventListener('load',main);
