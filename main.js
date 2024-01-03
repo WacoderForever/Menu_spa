d4b3d75d78954ddc3441e72f37050cc2dacf4fc3
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-01-03 11:52:36 -0300

    att

--- main.js
+++ main.js
@@ -4,68 +4,73 @@
 let main_interface;
 let chart = [];
 function start(){
+    let main_div = main_interface.div(()=>{
+        let first = true;
+        for(let char_item of chart){
 
-    let first = true;
-    for(let char_item of chart){
-
-        let item_div = main_interface.div(()=>{
+            let item_div = main_interface.div(()=>{
 
 
 
-            let select = main_interface.select(()=>{
+                let select = main_interface.select(()=>{
 
-                for(let id in ITENS){
-                    let current = ITENS[id];
-                    let current_option = main_interface.option(current.name);
-                    if(current === char_item.chosed){
-                        current_option.set_prop('selected',true);
+                    for(let id in ITENS){
+                        let current = ITENS[id];
+                        let current_option = main_interface.option(current.name);
+                        if(current === char_item.chosed){
+                            current_option.set_prop('selected',true);
+                        }
+
+                        current_option.set_prop('value',id);
                     }
+                });
 
-                    current_option.set_prop('value',id);
+                select.set_prop('APosition','$(80%,5%,15%,25%)')
+                select.set_prop('render_change',(selected)=>{
+                    char_item.chosed = ITENS[selected.value];
+                })
+
+                if(char_item.chosed){
+                    let my_image = main_interface.create('img');
+                    my_image.set_prop('APosition','$(5%,10%,30vh,30vh)');
+                    my_image.set_prop('src',char_item.chosed.picture);
+
+
                 }
+
             });
 
-            select.set_prop('APosition','$(80%,5%,15%,25%)')
-            select.set_prop('render_change',(selected)=>{
-                char_item.chosed = ITENS[selected.value];
-            })
+            item_div.inline_style({
+                'background-color':'rgb(192,192,192)',
+            });
 
-            if(char_item.chosed){
-                let my_image = main_interface.create('img');
-                my_image.set_prop('APosition','$(5%,10%,30vh,30vh)');
-                my_image.set_prop('src',char_item.chosed.picture);
-
-
+            if(first){
+                item_div.set_prop('APosition','$(5vw,10vh,50vw,40vh)');
+                first = false;
             }
 
-        });
+            else{
+                item_div.set_prop('APosition','$(5vw,+42vh,50vw,40vh)');
+            }
 
-        item_div.inline_style({
-            'background-color':'rgb(192,192,192)',
-        });
-
-        if(first){
-            item_div.set_prop('APosition','$(5vw,10vh,50vw,40vh)');
-            first = false;
         }
 
-        else{
-            item_div.set_prop('APosition','$(5vw,+42vh,50vw,40vh)');
-        }
-
-    }
 
 
+        let my_button = main_interface.button("Add");
+        my_button.set_prop('render_click',()=>{
+            const DEFAULT_CHART_ITEM = {
+                quantity:0,
+                chosed:undefined
+            }
+            chart.unshift(DEFAULT_CHART_ITEM);
+        });
+        my_button.set_prop('APosition','$(+55vw,+10vh,5vw,5vh)');
+    })
 
-    let my_button = main_interface.button("Add");
-    my_button.set_prop('render_click',()=>{
-          const DEFAULT_CHART_ITEM = {
-              quantity:0,
-              chosed:undefined
-          }
-          chart.unshift(DEFAULT_CHART_ITEM);
-    });
-    my_button.set_prop('APosition','$(+40vw,+32vh,5vw,5vh)');
+
+
+
 }
 function main(){
     main_interface = createElement404(start,document.body);
