be0e0d7782152707db87b4ed4357e65d004642a0
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-01-03 11:40:41 -0300

    att

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ let main_interface;
 let chart = [];
 function start(){
 
+    let first = true;
     for(let char_item of chart){
 
         let item_div = main_interface.div(()=>{
@@ -24,14 +25,14 @@ function start(){
                 }
             });
 
-            select.set_prop('APosition','$(70%,5%,15%,25%)')
+            select.set_prop('APosition','$(80%,5%,15%,25%)')
             select.set_prop('render_change',(selected)=>{
                 char_item.chosed = ITENS[selected.value];
             })
 
             if(char_item.chosed){
                 let my_image = main_interface.create('img');
-                my_image.set_prop('APosition','$(5%,5%,50%,90%)');
+                my_image.set_prop('APosition','$(5%,10%,30vh,30vh)');
                 my_image.set_prop('src',char_item.chosed.picture);
 
 
@@ -42,7 +43,16 @@ function start(){
         item_div.inline_style({
             'background-color':'rgb(192,192,192)',
         });
-        item_div.set_prop('APosition','$(5vw,+32vh,30vw,30vh)');
+
+        if(first){
+            item_div.set_prop('APosition','$(5vw,10vh,50vw,40vh)');
+            first = false;
+        }
+
+        else{
+            item_div.set_prop('APosition','$(5vw,+42vh,50vw,40vh)');
+        }
+
     }
 
 
@@ -55,7 +65,7 @@ function start(){
           }
           chart.unshift(DEFAULT_CHART_ITEM);
     });
-    my_button.set_prop('APosition','$(+20vw,+32vh,5vw,5vh)');
+    my_button.set_prop('APosition','$(+40vw,+32vh,5vw,5vh)');
 }
 function main(){
     main_interface = createElement404(start,document.body);
