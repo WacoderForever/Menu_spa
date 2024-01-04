4c027fe7d60452554e580d7a70bcd6a89d2ade25
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-01-04 18:34:29 -0300

    att

--- main.js
+++ main.js
@@ -5,15 +5,29 @@ let main_interface;
 
 
 function ShowItemOnInterface(item){
-    main_interface.div(()=>{
+   let div = main_interface.div(()=>{
        let myimage =main_interface.create("img")
        myimage.set_prop("src",item["image"])
-      // myimage.set_prop("APosition","$(100px,+100px,100px,100px)")
-    })
+       myimage.set_prop('APosition','$(5%,5%,90%,90%)');
+       myimage.inline_style({
+         'fit-content':'contain'
+       })
+
+
+   })
+
+   div.set_prop('APosition','$(10%,f34%,20%,30%)')
+   div.inline_style({
+      "background-color":"red"
+   })
 }
 
 function start(){
-   ITENS.forEach(ShowItemOnInterface)
+   let root = main_interface.div(()=>{
+      ITENS.forEach(ShowItemOnInterface)
+   })
+
+   root.set_prop('APosition','$(0%,0%,100%,100%)')
 
 
 }
