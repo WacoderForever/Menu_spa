e8bad469fbd39416d11d091bc02c303d2b3f6852
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-01-04 23:24:35 -0300

    att

--- itens.js
+++ itens.js
@@ -20,6 +20,47 @@ const ITENS = [
         "name":"pancakes",
         "image":"images/pancakes.jpg",
         "price":6.5
-    }
+    },
+    {
+        "name":"pasta",
+        "image":"images/pasta.jpg",
+        "price":5.5
+    },
 
+    {
+        "name":"feijoada",
+        "image":"images/feijoada.jpg",
+        "price":5 
+    },
+    {
+        "name":"sandwich",
+        "image":"images/sandwich.jpg",
+        "price":4
+    },
+    {
+        "name":"pancakes",
+        "image":"images/pancakes.jpg",
+        "price":6.5
+    },
+    {
+        "name":"pasta",
+        "image":"images/pasta.jpg",
+        "price":5.5
+    },
+
+    {
+        "name":"feijoada",
+        "image":"images/feijoada.jpg",
+        "price":5 
+    },
+    {
+        "name":"sandwich",
+        "image":"images/sandwich.jpg",
+        "price":4
+    },
+    {
+        "name":"pancakes",
+        "image":"images/pancakes.jpg",
+        "price":6.5
+    }
 ]
--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 let main_interface;
 
 
-function ShowItemOnInterface(item){
+function ShowItemOnInterface(item,index){
    let div = main_interface.div(()=>{
        let myimage =main_interface.create("img")
        myimage.set_prop("src",item["image"])
@@ -12,14 +12,22 @@ function ShowItemOnInterface(item){
        myimage.inline_style({
          'fit-content':'contain'
        })
-
+      
 
    })
-
-   div.set_prop('APosition','$(10%,f34%,20%,30%)')
+   let break_line = index % 3 == 0   
    div.inline_style({
       "background-color":"red"
    })
+   let same_line = !break_line
+   if(same_line){
+      div.set_prop('APosition','$(f33%,+0%,31%,31%)')
+   }
+
+   if(break_line){
+      div.set_prop('APosition','$(0%,f33%,31%,31%)')
+   }
+
 }
 
 
@@ -30,12 +38,25 @@ function ShowItemOnInterface(item){
 function start(){
    
    let root = main_interface.div(()=>{
-      ITENS.forEach(ShowItemOnInterface)
+
+      let sub = main_interface.div(()=>{
+         for(let i = 0; i < ITENS.length; i++){
+            ShowItemOnInterface(ITENS[i],i)
+         }
+      })
+      sub.set_prop('APosition','$(0%,0%,100vw,100vh)')
    })
 
-   root.set_prop('APosition','$(0%,0%,100%,100%)')
+
+   
+   root.inline_style({
+      'background-color':'black'
+   })
+
+   root.set_prop('APosition','$(0%,0%,100vw,1000vh)')
 
 
+   
 
 }
 
