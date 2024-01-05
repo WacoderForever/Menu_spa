a2cf01c95d1511765b8d87c93d7f4a27902c57af
Wacoder Forever <sethomondi088@gmail.com>
2024-01-05 17:15:44 +0300

    mod2

--- main.js
+++ main.js
@@ -4,165 +4,162 @@
 let main_interface;
 let bil_div;
 
-function calculate_and_show_bill() {
+function calculate_and_show_bill(){
    bil_div.clear();
-   let result = 0;
-
-   ITENS.forEach((value) => {
+   let result  = 0;
+   
+   ITENS.forEach((value)=>{
       result += value.price * value.quantity;
    })
 
 
-   let bil_paragraph = bil_div.p(result)
-
+   let bil_paragraph = bil_div.p(result)        
+   
    bil_paragraph.inline_style({
-      'color': 'orange',
-      'font-size': '2em'
-   })
+      'color':'orange',
+      'font-size':'2em'
+   })     
 
 
 }
 
-function changeInputValue(input, item) {
-   if (input.value < 0) {
-      input.value = 0
+function changeInputValue(input,item){
+   if(input.value <0){
+      input.value=0
    }
    item.quantity = input.value
    calculate_and_show_bill()
 }
-
-function ItemContent(item) {
-   let content_div = main_interface.div(() => {
+function ItemContent(item){
+   let content_div = main_interface.div(()=>{
 
       let title = item["name"] + "  $" + item["price"]
       let name = main_interface.p(title);
 
-
+   
       let input = main_interface.input();
       input.inline_style({
-         'width': '80%',
-         'height': '20%',
-         'background-color': '#333',
-         'color': 'orange',
-         'font-size': '1em',
+         'width':'80%',
+         'height':'20%',
+         'background-color':'#333',
+         'color':'orange',
+         'font-size':'1em',
+      })  
+
+      input.set_prop('change',(input)=>{
+         changeInputValue(input,item)
       })
+   
+   input.set_prop('value',0)
+   input.set_prop('type','number')
 
-      input.set_prop('change', (input) => {
-         changeInputValue(input, item)
-      })
+})
+ content_div.inline_style({
+   'color':'orange',
+   'font-size':'2em',
+   'text-align':'center'
+})
 
-      input.set_prop('value', 0)
-      input.set_prop('type', 'number')
-
-   })
-   content_div.inline_style({
-      'color': 'orange',
-      'font-size': '2em',
-      'text-align': 'center'
-   })
-
-   content_div.set_prop('APosition', '$(40%,3vh,60%,20vh)');
+ content_div.set_prop('APosition','$(40%,3vh,60%,20vh)');
 }
 
-function BreakLine(index) {
-   let break_line = index % 3 == 0
+
+
+function ShowItemOnInterface(item,index){
+   let div = main_interface.div(()=>{
+       let myimage =main_interface.create("img")
+       myimage.set_prop("src",item["image"])
+       myimage.set_prop('APosition','$(3vh,3vh,20vh,20vh)');
+       myimage.inline_style({
+         'fit-content':'contain'
+       })
+      let content=main_interface.div(ItemContent(item))
+  
+
+   })
+ 
+   div.inline_style({
+      'fit-content':'contain',
+      'border': '1px solid orange' 
+   })
+
+   let break_line = index % 3 == 0  
 
    let same_line = !break_line
-   if (same_line) {
-      div.set_prop('APosition', '$(f33%,+0%,31%,31%)')
+   if(same_line){
+      div.set_prop('APosition','$(f33%,+0%,31%,31%)')
    }
 
-   if (break_line) {
-      div.set_prop('APosition', '$(0%,f33%,31%,31%)')
+   if(break_line){
+      div.set_prop('APosition','$(0%,f33%,31%,31%)')
    }
 
 }
 
 
-function ShowItemOnInterface(item, index) {
-   let div = main_interface.div(() => {
-      let myimage = main_interface.create("img")
-      myimage.set_prop("src", item["image"])
-      myimage.set_prop('APosition', '$(3vh,3vh,20vh,20vh)');
-      myimage.inline_style({
-         'fit-content': 'contain'
-      })
-      ItemContent(item)
 
-   })
-
-   div.inline_style({
-      'fit-content': 'contain',
-      'border': '1px solid orange'
-   })
-
-   BreakLine(index)
-}
-
-
-
-function createRootContent() {
-   let sub = main_interface.div(() => {
-
-      let nav = main_interface.nav(() => {
+function createRootContent(){
+   let sub = main_interface.div(()=>{
+         
+      let nav = main_interface.nav(()=>{
 
          bil_div = main_interface.div();
          calculate_and_show_bill()
 
+       
 
-
-         bil_div.set_prop('APosition', '$(90%,0%,100%,10%)')
+         bil_div.set_prop('APosition','$(90%,0%,100%,10%)')
 
 
       })
       nav.inline_style({
-         'background-color': '#333',
+         'background-color':'#333',
       })
 
-      nav.set_prop('APosition', '$(0%,0%,100%,10%)')
+      nav.set_prop('APosition','$(0%,0%,100%,10%)')
 
 
-      let my_content = main_interface.div(() => {
-         for (let i = 0; i < ITENS.length; i++) {
-            ShowItemOnInterface(ITENS[i], i)
-         }
+      let my_content = main_interface.div(()=>{
+         for(let i = 0; i < ITENS.length; i++){
+            ShowItemOnInterface(ITENS[i],i)
+         }         
       })
-      my_content.set_prop('APosition', '$(0%,15%,100%,85%)')
+      my_content.set_prop('APosition','$(0%,15%,100%,85%)')
 
    })
-   sub.set_prop('APosition', '$(0%,0%,100vw,100vh)')
+   sub.set_prop('APosition','$(0%,0%,100vw,100vh)')
 }
 
-function start() {
-
+function start(){
+   
    let root = main_interface.div(createRootContent)
 
 
-
+   
    root.inline_style({
-      'background-color': 'black'
+      'background-color':'black'
    })
 
-   root.set_prop('APosition', '$(0%,0%,100vw,1000vh)')
+   root.set_prop('APosition','$(0%,0%,100vw,1000vh)')
 
 
-
+   
 
 }
 
 
 
 
-function main() {
+function main(){
 
-   ITENS.forEach((value) => {
-      value.quantity = 0
+   ITENS.forEach((value)=>{
+      value.quantity=0
    })
-   main_interface = createElement404(start, document.body);
-   main_interface.render();
-   // start()
+   main_interface = createElement404(start,document.body);
+      main_interface.render();
+  // start()
 
 }
 
 
-window.addEventListener('load', main);
+window.addEventListener('load',main);
